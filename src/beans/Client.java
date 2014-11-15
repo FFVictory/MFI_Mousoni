430a6e26b0c1ac196d877e377a261703839f5067
Aleksejs Kudrins <alexthe228@gmail.com>
2014-11-15 01:02:39 +0000

    Client bean created

--- src/beans/Client.java
+++ src/beans/Client.java
@@ -1,7 +1,46 @@
 package beans;
 
+import java.util.Date;
+
 /**
  * Created by Sawa-Kvas on 15/11/2014.
  */
 public class Client {
+
+    Date date_of_birth;
+    int status;
+    String fullname;
+
+    public Date GetDOB()
+    {
+        return date_of_birth;
+    }
+
+    public int GetStatus()
+    {
+        return status;
+    }
+
+    public String GetFullname()
+    {
+        return fullname;
+    }
+
+    public void SetDOB(Date DOB)
+    {
+        this.date_of_birth = DOB;
+    }
+
+    public void SetStatus(int status)
+    {
+        this.status = status;
+    }
+
+    public void SetFullName(String fullname)
+    {
+        this.fullname = fullname;
+    }
+
+
+
 }
